57409cc1d95f171049f8f6f861a81327501d4a36
Maharani Nirmala Dewi <maharanisakti015@Gmail.com>
2022-08-13 13:31:55 +0700

    first commit

---
+++ app/src/main/java/com/mahar/notificationapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mahar.notificationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
