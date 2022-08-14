5ebe876c9b9c71b454920d6c77ec5e7d8b71e3b4
Maharani Nirmala Dewi <maharanisakti015@Gmail.com>
2022-08-14 14:59:33 +0700

    notification_procedurs done

--- app/src/main/java/com/mahar/notificationapp/MainActivity.java
+++ app/src/main/java/com/mahar/notificationapp/MainActivity.java
@@ -8,6 +8,10 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,18 +43,45 @@ public class MainActivity extends AppCompatActivity {
         Intent i=new Intent(this,MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,0);
 
+        //        action toast message
+        Intent actionIntent=new Intent(this,receivers.class);
+        actionIntent.putExtra("toast","this is notification Message");
+        PendingIntent actionPending=PendingIntent.getBroadcast(this,0,actionIntent,0);
+
+        Notification.Action action=new Notification.Action.Builder(Icon.createWithResource(this,R.drawable.ic_notif)
+                ,"Toast Message",actionPending).build();
+//          action dismiss
+
+        Intent dismissIntent=new Intent(this,receiversDismiss.class);
+        PendingIntent dismissPending=PendingIntent.getBroadcast(this,0,dismissIntent,0);
+
+        Notification.Action dismiss=new Notification.Action.Builder(Icon.createWithResource(this,R.drawable.ic_notif)
+                ,"Dismiss Message",dismissPending).build();
+
+
         NotificationChannel channel= new NotificationChannel(CHANNEL_ID,"1"
                 , NotificationManager.IMPORTANCE_DEFAULT);
 
         NotificationManager manager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         manager.createNotificationChannel(channel);
 
+        Bitmap icon= BitmapFactory.decodeResource(getResources()
+                ,R.drawable.girl);
+        String text=getResources().getString(R.string.big_text);
+
         Notification.Builder builder= new Notification.Builder(MainActivity.this,CHANNEL_ID);
         builder.setSmallIcon(R.drawable.ic_notif)
                 .setContentTitle("Judul Notification")
                 .setContentText("Text Notification")
                 .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .addAction(action)
+                .addAction(dismiss)
+                .setColor(Color.BLUE)
+                .setLargeIcon(icon);
+//                .setStyle(new Notification.BigTextStyle().bigText(text));
+//                .setStyle(new Notification.BigPictureStyle().bigPicture(icon));
+//                .setActions(action);
 //                .setPriority(Notification.PRIORITY_DEFAULT);
 
         NotificationManagerCompat compat=NotificationManagerCompat.from(MainActivity.this);
---
+++ app/src/main/java/com/mahar/notificationapp/receiversDismiss.java
@@ -0,0 +1,15 @@
+package com.mahar.notificationapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationManagerCompat;
+
+public class receiversDismiss extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManagerCompat compat=NotificationManagerCompat.from(context);
+        compat.cancel(1);
+    }
+}
